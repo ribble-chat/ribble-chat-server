eb282ded286e3ae8dbbd567e610745ab782fc5f2
Andy Yu <andyyu2004@gmail.com>
2021-02-07 17:10:53 +1300

    make login fields non-nullable; add migrations

--- Models/User.cs
+++ Models/User.cs
@@ -5,35 +5,35 @@ namespace RibbleChatServer.Models
 {
     public class User : IdentityUser<int>
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
     }
 
     public record RegisterUserInfo
     {
         [Required]
-        public string? Email { get; set; }
+        public string Email { get; set; } = null!;
 
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; } = null!;
 
         [Required]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; } = null!;
 
         [Required]
-        public string? LastName { get; set; }
+        public string LastName { get; set; } = null!;
 
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; } = null!;
     }
 
     public class LoginUserInfo
     {
         [Required]
-        public string? UsernameOrEmail { get; set; }
+        public string UsernameOrEmail { get; set; } = null!;
 
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; } = null!;
     }
 }
 
